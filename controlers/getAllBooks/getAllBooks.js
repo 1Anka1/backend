0623c59468af2409ee219e92e9bb2f141e5b378d
Anna Kasianenko <annakasjanenko02@gmail.com>
2025-10-07 18:49:21 +0200

    refactoring

--- controlers/getAllBooks/getAllBooks.js
+++ controlers/getAllBooks/getAllBooks.js
@@ -1,12 +1,11 @@
 const { default: axios } = require('axios');
 const { Book } = require('../../models/book');
-const { BASE_URL, API_KEY } = process.env;
 
-const getAllBooks = async (rec, res) => {
-  const books = await axios.get(
-    `${BASE_URL}/v3/lists/overview.json?api-key=${API_KEY}`
-  );
-  await Book.insertMany(books);
+const getAllBooks = async (_, res) => {
+  // const books = await axios.get(
+  //   `${BASE_URL}/v3/lists/overview.json?api-key=${API_KEY}`
+  // );
+  // await Book.insertMany(books);
   const allBooks = await Book.find({});
 
   res.status(200).json({ message: 'Successful', data: allBooks });
